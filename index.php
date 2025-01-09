bb3f493228819ea9401903cf7667a6dc0dcba53a
RoyLin <orangeskyonline@gmail.com>
2025-01-09 09:17:09 +0800

    改CSS,JS路徑與刪除不要的部分

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
-﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+﻿﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0047)? -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>影城</title>
 <link rel="stylesheet" href="css/css.css">
-<link href="home_files/s2.css" rel="stylesheet" type="text/css">
-<script src="scripts/jquery-1.9.1.min.js"></script>
+<link href="css/s2.css" rel="stylesheet" type="text/css">
+<script src="js/jquery-3.4.1.min.js"></script>
 </head>
 
 <body>
@@ -21,28 +21,20 @@
     </marquee>
   </div>
   <div id="mm">
-    <div class="half" style="vertical-align:top;">
-      <h1>預告片介紹</h1>
-      <div class="rb tab" style="width:95%;">
-        <div id="abgne-block-20111227">
-          <ul class="lists">
-          </ul>
-          <ul class="controls">
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div class="half">
-      <h1>院線片清單</h1>
-      <div class="rb tab" style="width:95%;">
-        <table>
-          <tbody>
-            <tr> </tr>
-          </tbody>
-        </table>
-        <div class="ct"> </div>
-      </div>
-    </div>
+  <?php 
+  
+    $do=$_GET['do']??'main';
+    $file="./front/$do.php";
+
+    if(file_exists($file)){
+      include $file;
+    }else{
+      include "./front/main.php";
+    }
+  
+    
+  
+  ?>
   </div>
   <div id="bo"> ©Copyright 2010~2014 ABC影城 版權所有 </div>
 </div>
